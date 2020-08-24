8a5aab1d82941d678b8e4c75ed9b1e2b1fe3f3fb
Molina60217N2 <65309678+Molina60217N2@users.noreply.github.com>
2020-08-24 13:23:22 -0600

    Add files via upload

---
+++ Contenedor.cpp
@@ -0,0 +1,38 @@
+#include "Contenedor.h"
+Contenedor obj; 
+int main() {
+	int opc; 
+	do{
+	cout << "Ingrese una opcion" << endl; 
+	cout << "1)Crear arreglo" << endl;
+	cout << "2)Ingresar datos para el arreglo" << endl; 
+	cout << "3)Mostrar" << endl;
+	cout << "4)Eliminar" << endl; 
+	cin>>opc; 	
+		
+		
+	switch(opc){
+		
+	case 1: 
+	obj.crear();	
+	break; 
+	case 2: 
+		obj.ingresar();
+		break;
+	case 3: 
+		obj.mostrar();
+		break; 
+	case 4: 
+		obj.eliminar();
+		break; 
+		
+		
+	}
+		
+	} while(opc != 5);
+	
+	
+	
+
+}
+
---
+++ Contenedor.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+class Contenedor{
+private: 
+	int* ptrVector;
+	int tamanio;
+	int cantidad; 
+public: 
+	Contenedor();
+	~Contenedor();
+	void crear();
+	void ingresar();
+	void mostrar();
+	void eliminar();
+};
+
+Contenedor::Contenedor(){
+	cantidad = 0; 
+	ptrVector = NULL; 
+	tamanio = 0; 
+}
+Contenedor::~Contenedor(){
+	
+}
+void Contenedor::crear(){
+	cout << "Ingrese el tamanio para el arreglo: " << endl; 
+	cin >> tamanio; 
+	ptrVector = new int[tamanio]; 
+	for(int i = 0; i < tamanio; i++){
+		ptrVector[i] = 0; 
+	} 
+}
+void Contenedor::ingresar(){
+	for(int i = 0; i < tamanio; i++){
+		cout << "Ingrese el valor N" << i+1 << ": " << endl; 
+		cin>> ptrVector[i]; 
+		cantidad ++; 
+	}
+	
+}
+void Contenedor::mostrar(){
+	for(int i = 0; i < tamanio; i++){
+		cout << "\nEl valor N" << i << " es: " << ptrVector[i] << endl; ; 
+	}
+	
+}
+void Contenedor::eliminar(){
+	delete[] ptrVector; 
+	ptrVector = NULL;
+	cantidad = 0; 
+}
